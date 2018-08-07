df4067b919280de8a822140bccdaa973d107d196
kkeertipati <karthik@KARTHIKs-MBP.fios-router.home>
2018-08-07 10:11:58 -0400

    add

--- README.md
+++
@@ -1,4 +0,0 @@
-# TODO-webapp-using-PHP-and-SQL
-A class project to develop database and build a website to perform several functions. 
-
-Check out my project website here: http://studentweb.comminfo.rutgers.edu/class-2017-9-17-610-557-91/kk868/Final%20Project/index.php
---
+++ templates/header.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+include_once 'dbconnect.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home | Koding Made Simple</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" >
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+<style>
+
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+
+tr:nth-child(even) {
+    background-color: #dddddd;
+}
+</style>
+</head>
+
+
+    
+
+
+<body>
+
+<nav class="navbar navbar-default" role="navigation">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="home.php">DLOG </a>
+        </div>
+        <div class="collapse navbar-collapse" id="navbar1">
+            <ul class="nav navbar-nav navbar-right">
+                <?php if (isset($_SESSION['usr_id'])) { ?>
+                <li><p class="navbar-text">Signed in as <?php echo $_SESSION['usr_name']; ?></p></li>
+                <li><a href="logout.php">Log Out</a></li>
+                <?php } else { ?>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="register.php">Sign Up</a></li>
+                <?php } ?>
+            </ul>
+        </div>
+    </div>
+</nav>    
